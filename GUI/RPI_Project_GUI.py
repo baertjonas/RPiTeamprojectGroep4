5cd270716a8a3fc597de85357a271ccbd8b0e8a0
Joey Jansegers <joey.jansegers@student.ap.be>
2020-05-07 13:05:45 +0200

    Edits aan GUI

--- GUI/RPI_Project_GUI.py
+++ GUI/RPI_Project_GUI.py
@@ -2,21 +2,21 @@
 
 import tkinter as tk
 import paho.mqtt.client as mqtt
-from PIL import ImageTk, Image
 from threading import Thread
+import time
 
 listPlayersPos = []
 listPlayersID = []
 flagStartDestroy = 0
 
-imgWCrol = ImageTK.PhotoImage(Image.open("Toilet_paper.png"))
-imgWCrol = imgWCrol.resize((150,150), Image.ANTIALIAS)
-imgVirus = ImageTK.PhotoImage(Image.open("virus.png"))
-imgVirus = imgVirus.resize((150,150), Image.ANTIALIAS)
-imgCart = ImageTK.PhotoImage(Image.open("Cart.png"))
-imgCart = imgCart.resize((150,150), Image.ANTIALIAS)
-
 venster = tk.Tk()
+
+imgWCrol = tk.PhotoImage( file = "Toilet_paper.png" )
+
+imgVirus = tk.PhotoImage( file = "virus.png")
+
+imgCart = tk.PhotoImage( file = "Cart.png" )
+
 venster.title('RPi Groep 4')
 venster.geometry("1600x900")
 
@@ -29,7 +29,7 @@ def on_message(client, userdata, msg):
     payload = str(msg.payload)
     global listPlayersPos
     #ID=0; X=0100; Y=0100;
-    #0 = Rol; 1 = Virus; 2 = cart;
+    #0 = Cart; 1 = Virus; 2 = WCrol;
     #TODO: Scrape payload from ID, X and Y
     ID = 0
     Xpos= 100
@@ -40,28 +40,24 @@ def on_message(client, userdata, msg):
 def on_subscribe(client, userdata, mid, granted_qos):
     print("Subscribed: " + str(mid) + " QoS: " + str(granted_qos))
 
-
-
 def StartMQTT():
     global client
     client.loop_forever()
 
 def GUI():
-    global flagStartDestroy
-    if flagStartDestroy == 1:
-        frame.destroy()
-    frame = Frame(venster)
-    flagStartDestroy = 1
-    for x in (len(listPlayersID) - 1):
+    frame = tk.Frame(venster)
+    global listPlayersID, listPlayersPos
+    aantalIteraties = len(listPlayersID) - 1
+    for x in range(0, aantalIteraties):
         if listPlayersID[x] == 0:
-            Img = Label(frame, image = imgWCrol)
-        elif listPlayersID[x] == 1:
-            Img = Label(frame, image = imgVirus)
-        elif listPlayersID[x] == 2:
-            Img = Label(frame, image = imgCart)
+            Img = tk.Label(frame, image = imgCart)
+        elif listPlayersID[x] % 2:
+            Img = tk.Label(frame, image = imgVirus)
+        else:
+            Img = tk.Label(frame, image = imgWCrol)
+        Img.pack()
         Img.place(x=listPlayersPos[x][0], y=listPlayersPos[x][1])
-
-
+    frame.destroy()
 
 client = mqtt.Client(client_id="clientId-x3sRNOeZi9", clean_session=True, userdata=None, protocol=mqtt.MQTTv31)
 client.on_connect = on_connect
@@ -71,5 +67,7 @@ client.connect("broker.mab3lly.rocks", 1883)
 
 job1= Thread(target=StartMQTT)
 job1.start()
-
+time.sleep(3)
+job2 = Thread(target=GUI)
+job2.start()
 
