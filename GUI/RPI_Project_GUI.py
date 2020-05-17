0ec29c84970a5fc0a7ce4cdd24554801a25bcc94
Joey <joey.jansegers@student.ap.be>
2020-05-17 16:01:56 +0200

    GUI up and running

--- GUI/RPI_Project_GUI.py
+++ GUI/RPI_Project_GUI.py
@@ -7,12 +7,7 @@ import time
 
 listPlayersPos = []
 listPlayersID = []
-flagStartDestroy = 0
-ID = 0
-Xpos= 100
-Ypos= 100
-listPlayersID.append(ID)
-listPlayersPos.append([Xpos, Ypos])
+deleteImages = 0
 
 def MQTT():
 
@@ -21,12 +16,15 @@ def MQTT():
         client.subscribe("RPi/GUI")
 
     def on_message(client, userdata, msg):
-        print("Message received: " + str(msg.payload))
+        print(str(msg.payload))
         payload = str(msg.payload)
-        Slice_ID = slice(4,5)
-        Slice_xpos = slice(10,13) 
-        Slice_ypos = slice(18,21)
+        Slice_ID = slice(6,7)
+        Slice_xpos = slice(12,15) 
+        Slice_ypos = slice(20,23)
         global listPlayersPos, listPlayerID
+        print(str(payload[Slice_ID]))
+        print(str(payload[Slice_xpos]))
+        print(str(payload[Slice_ypos]))
         listPlayersID.append(payload[Slice_ID])
         listPlayersPos.append([payload[Slice_xpos], payload[Slice_ypos]])
         #ID=00; X=0100; Y=0100;
@@ -44,36 +42,43 @@ def MQTT():
 
 def GUI():
     venster = tk.Tk()
-
     imgWCrol = tk.PhotoImage(file="Toilet_paper.png")
-
     imgVirus = tk.PhotoImage(file="virus.png")
-
     imgCart = tk.PhotoImage(file="Cart.png")
-
     venster.title('RPi Groep 4')
     canvas = tk.Canvas(venster, width=800, height=600)
     canvas.pack()
 
+    def updateScreen():
+        global listPlayersID, listPlayersPos, deleteImages
+        if deleteImages == 1:
+            canvas.delete("all")
+        deleteImages = 1
+        aantalIteraties = len(listPlayersID)
+        for x in range(0, aantalIteraties):
+            if listPlayersID[x] == 0:
+                image = canvas.create_image(listPlayersPos[x][0], listPlayersPos[x][1], anchor=tk.N, image=imgCart)
+            elif int(listPlayersID[x]) % 2 == 0:
+                image = canvas.create_image(listPlayersPos[x][0], listPlayersPos[x][1], anchor=tk.N, image=imgWCrol)
+            else:
+                image = canvas.create_image(listPlayersPos[x][0], listPlayersPos[x][1], anchor=tk.N, image=imgVirus)
+        listPlayersPos = []
+        listPlayersID = []
+        venster.after(1000, updateScreen)
+    
+    updateScreen()
+    venster.mainloop()
+    #image = canvas.create_image(100, 100, anchor=tk.W, image=imgWCrol)
+
     global listPlayersID, listPlayersPos
     aantalIteraties = len(listPlayersID)
 
-    for x in range(0, aantalIteraties):
-        if listPlayersID[x] == 0:
-            image = canvas.create_image(listPlayersPos[x][0], listPlayersPos[x][1], anchor=tk.N, image=imgCart)
-        if listPlayersID[x] % 2:
-            image = canvas.create_image(listPlayersPos[x][0], listPlayersPos[x][1], anchor=tk.E, image=imgVirus)
-        else:
-            image = canvas.create_image(listPlayersPos[x][0], listPlayersPos[x][1], anchor=tk.W, image=imgWCrol)
-    
-    venster.mainloop()
-    #image = canvas.create_image(100, 100, anchor=tk.W, image=imgWCrol)
-    
-    
-
-
-
-job1 = Thread(target=GUI)
-job2 = Thread(target=MQTT)
-job1.start()
-job2.start()
+try:
+    job1 = Thread(target=GUI)
+    job2 = Thread(target=MQTT)
+    job1.start()
+    job2.start()
+except KeyboardInterrupt:
+    job1.stop()
+    job2.stop()
+    pass
