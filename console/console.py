3922e57cccc8a0e15a9640c1c8f842b268a055e9
Jonas Baert <jonas.baert@student.ap.be>
2020-05-24 13:41:20 +0200

    getting the bugs out

--- console/console.py
+++ console/console.py
@@ -16,6 +16,7 @@ def randomString(stringLength):
     return ''.join(random.choice(letters) for i in range(stringLength))
 
 clientID = randomString(8)
+print("Je clientID is "+clientID)
 
 def on_connect(client, userdata, flags, rc):
  print("Connected with result code " + str(rc))
@@ -29,29 +30,31 @@ def on_message(client, userdata, msg):
  if((str(msg.payload)[slice(2,7)]) == "GetID"):
   print(str(msg.payload)[slice(8,10)])
   ID = (str(msg.payload)[slice(8,10)])
+  print("Je (nieuwe) ID is " +str(ID))
+  GPIO.output(leds, 0) # alle leds uit!!!
   if (int(ID)==0):
-      print("Je bent nu een winkelkar.")
+      print("Je bent een winkelkar.")
       GPIO.output(leds[1], 1) # gele led
   elif(int(ID) % 2 == 0):
-      print ("Je bent nu een wc-rol.")
+      print ("Je bent een wc-rol.")
       GPIO.output(leds[0], 1) # rode led
   elif(int(ID) % 2 != 0):
-      print("Je bent nu een virus.")
+      print("Je bent een virus.")
       GPIO.output(leds[2], 1) # groene led
  elif((str(msg.payload)[slice(2,7)]) == "Rspwn"):
   currentID=(str(msg.payload)[slice(8,10)])
   newID=(str(msg.payload)[slice(15,17)])
   Respawn(currentID,newID)
  GPIO.output(segments, BCD[int(ID)])
- 
+
 def GetID():
  client.publish("RPi/Controller", "GetID; Client=" + clientID)
- 
+
 def Respawn(currentID, newID):
  global ID
  if(currentID == ID):
   ID=newID
- 
+
 def icb(channel):
   global ID
   if channel == buttons[0]:
@@ -60,7 +63,7 @@ def icb(channel):
   if channel == buttons[1]:
     ret = client.publish("RPi/Controller", "ID=" + ID +"; Action=DN")
     print("DOWN")
- 
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(leds, GPIO.OUT) # leds activeren
 GPIO.output(leds, False)
@@ -81,7 +84,7 @@ BCD = {0:(1,1,1,1,1,1,0),
     7:(1,1,1,0,0,0,0),
     8:(1,1,1,1,1,1,1),
     9:(1,1,1,1,0,1,1)}
- 
+
 client = mqtt.Client(client_id=clientID, clean_session=True)
 client.connect("broker.mab3lly.rocks",1883)
 client.on_connect = on_connect
@@ -92,14 +95,14 @@ client.loop_start()
 
 time.sleep(2)
 while ID is None:
- print(clientID)
  GetID()
  time.sleep(5)
 
-while True:
- print(ID)
- time.sleep(1)
+#while True:
+# print(ID)
+# time.sleep(1)
 
+print("Druk op return om de console af te sluiten...")
 input()
 
 for x in buttons:
